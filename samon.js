dc7b9086ecc1feec2bc22b8545a0bb07c4f255cb
Rojit tech 69 <rojitrajbanshi69@gmail.com>
2026-06-28 12:21:21 +0545

    Refactor event listeners and variable names

--- samon.js
+++ samon.js
@@ -1,27 +1,21 @@
-document.querySelector("#green").addEventListener("click", function () {
-    var audio= new Audio("sounds/green.mp3");
-audio.play()});
-document.querySelector("#red").addEventListener("click", function () {
-    var audio1= new Audio("sounds/red.mp3");
-audio1.play()});
-document.querySelector("#blue").addEventListener("click", function () {
-    var audio2= new Audio("sounds/blue.mp3");
-    audio2.play()});
-document.querySelector("#yellow").addEventListener("click", function () {
-    var audio3= new Audio("sounds/yellow.mp3");
-    audio3.play()});
-    var gamePattern=[];
-    var  buttoncolor=["red","green","yellow","blue"];
-function nextsequence(){
-    var randomnumber=(Math.floor(Math.random()*4));
-    var randomchossecolor =buttoncolor[randomnumber];
-gamePattern.push(randomchossecolor);
-}
-document.querySelector("#" + randomchosencolor)
+var gamePattern = [];
+var buttonColors = ["red", "green", "yellow", "blue"];
+
+function nextSequence() {
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+
+    gamePattern.push(randomChosenColor);
+
+    document.querySelector("#" + randomChosenColor)
         .classList.add("pressed");
 
-setTimeout(function () {
-    document.querySelector("#" + randomchosencolor)
+    setTimeout(function () {
+        document.querySelector("#" + randomChosenColor)
             .classList.remove("pressed");
-}, 100);
- 
+    }, 100);
+
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
+}
